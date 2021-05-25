c7125f7059cf460efb50d1897a03165ada7f5c19
Hemanth Mudra <mudra.hemanth@gmail.com>
2021-05-26 01:44:37 +0530

    selection sort + bubble sort

---
+++ Arrays/sorting.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int n, int arr[]) //O(n^2)
+{
+   for (int i = 0; i < n - 1; i++)
+   {
+      for (int j = i; j < n; j++)
+      {
+         if (arr[j] < arr[i])
+         {
+            // int temp = arr[j];
+            // arr[j] = arr[i];
+            // arr[i] = temp;
+            swap(arr[i],arr[j]);
+         }
+      }
+   }
+   for (int i = 0; i < n; i++)
+   {
+      cout << arr[i] << " ";
+   }
+}
+
+void bubbleSort(int n, int arr[])
+{
+   int counter = 1;
+   while (counter < n )
+   {
+      for (int i = 0; i < n-counter; i++)
+      {
+         if (arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+      }
+      counter++;
+   }
+   for (int i = 0; i < n; i++)
+   {
+      cout << arr[i] << " ";
+   }
+}
+
+int main()
+{
+   int n;
+   cin >> n;
+
+   int arr[n];
+
+   for (int i = 0; i < n; i++)
+   {
+      cin >> arr[i];
+   }
+
+   // selectionSort(n, arr);
+   // bubbleSort(n, arr);
+   return 0;
+}
