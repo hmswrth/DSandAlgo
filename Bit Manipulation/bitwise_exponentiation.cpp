71dcbadc1229895e46693f1c77b85bb4f65d50b7
Hemanth Mudra <mudra.hemanth@gmail.com>
2021-06-05 22:43:55 +0530

    update

---
+++ Bit Manipulation/bitwise_exponentiation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define l1 long long int
+#define MOD 1000000007
+
+l1 bexpo(l1 base, l1 n)
+{
+   l1 ans = 1;
+   while (n > 0)
+   {
+      if (n % 2 == 1)
+         ans = (ans * base) % MOD;
+      base = (base * base) % MOD;
+      n /= 2;
+   }
+   return ans;
+}
+
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   l1 t;
+   cin >> t;
+   for (l1 i = 0; i < t; i++)
+   {
+      l1 n, k;
+      cin >> n >> k;
+      cout << bexpo(n, k) << endl;
+   }
+
+   return 0;
+}
