be863a61a5c67d58d2d10733bf06b8bc6274b457
Hemanth Mudra <mudra.hemanth@gmail.com>
2021-06-06 12:41:52 +0530

    most freq char in string

---
+++ strings/max_freq_char.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void freq(string s)
+{
+   int freq[26];
+   for (int i = 0; i < 26; i++)
+      freq[i] = 0;
+
+   for (int i = 0; i < s.size(); i++)
+      freq[s[i] - 'a']++;
+
+   int maxFreq = 0;
+   char ans = 'a';
+
+   for (int i = 0; i < 26; i++)
+   {
+      if (freq[i] > maxFreq)
+      {
+         maxFreq = freq[i];
+         ans = 'a' + i;
+      }
+   }
+
+   cout << ans << " " << maxFreq << endl;
+}
+
+int main()
+{
+   string s = "abkajakjbblknalvnakjaskabbbbb";
+   freq(s);
+   return 0;
+}
