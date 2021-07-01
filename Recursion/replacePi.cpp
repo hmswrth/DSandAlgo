e44f3c18ca6b0712190ab0d4688b0396bf05a7ee
Hemanth Mudra <mudra.hemanth@gmail.com>
2021-07-02 00:57:24 +0530

    replace text using recursion

---
+++ Recursion/replacePi.cpp
@@ -0,0 +1,24 @@
+// Given a string, replace "pi" with "3.14" using recursion
+#include<bits/stdc++.h>
+using namespace std;
+
+void replacePi(string s){
+   if(s.length() == 0) //base case
+      return;
+
+   if(s[0] == 'p' && s[1] == 'i'){
+      cout<<"3.14";
+      replacePi(s.substr(2));
+   }
+   else{
+      cout<<s[0];
+      replacePi(s.substr(1));
+   }
+}
+
+int main(){
+   string s;
+   cin>>s;
+   replacePi(s);
+   return 0;
+}
